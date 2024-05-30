cba17806b1e7cffc0c889b7c14e7e2bda78fda0d
NemontovMaksim <149705847+NemontovMaksim@users.noreply.github.com>
2024-05-30 13:58:41 +0300

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,5 +1,13 @@
-const allowedCors = ['https://practicum.yandex.ru', 'https://students-projects.ru', 'http://localhost:3001', 'http://localhost:3000', 'http://mnemontov-backend.nomorepartiesco.ru', 'https://mnemontov-backend.nomorepartiesco.ru', 'https://mnemontov-front.nomorep.nomorepartiesco.ru'];
+// const allowedCors = ['https://practicum.yandex.ru', 
+//                      'https://students-projects.ru', 
+//                      'http://localhost:3001', 
+//                      'http://localhost:3000', 
+//                      'http://mnemontov-backend.nomorepartiesco.ru', 
+//                      'https://mnemontov-backend.nomorepartiesco.ru', 
+//                      'https://mnemontov-front.nomorep.nomorepartiesco.ru'];
 
+
+const allowedCors = [ 'https://mnemontov-front.nomorep.nomorepartiesco.ru'];
 // Функция, которая принимает объекты req (информация о запросе),
 // res (объект ответа) и функцию next (для запуска следующего миддлвара)
 function cors(req, res, next) {
